e480866e17264f21473c7e24d0bab4ce25123fca
PromotezCitizen <gkstkdgus821@gmail.com>
2025-05-22 18:02:39 +0900

    전반적인 로직 수정
    - 다운로드는 네트워크 I/O -> 스레드를 늘려도 무방
    - 이미 인코딩된 영상을 받는 로직이므로 ffmpeg의 -c 인자를 copy로 전달
    - 인코딩된 영상 분할 저장은 gpu를 쓰지 않으므로 -hwaccel 제거
    - 네트워크 에러가 뜨는 경우 지원하지 않는 링크로 판단

--- python/missav_downloader/missav_downloader_local.py
+++ python/missav_downloader/missav_downloader_local.py
@@ -4,26 +4,38 @@ from bs4 import BeautifulSoup
 from cloudscraper import CloudScraper, create_scraper
 import requests
 import os
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed, ProcessPoolExecutor
 from tqdm import tqdm
 import subprocess
-import gc
+import threading
 import time
+import io
+from urllib.parse import urlparse
 
 INFO_DOMAIN = 'surrit.com'
 NODEJS_INTERPRETER = 'bun'
-HWA_ACCEL = 'cuda'
+UNCENSORED_TAG = '-uncensored-leak'
 
 class MissavDownloader():
-    def __init__(self):
+    def __init__(self, is_mul_proc: bool = False):
         self.scraper: CloudScraper = create_scraper(
-            browser='chrome',
+            browser={
+                'browser': 'chrome',
+                'platform': 'android',
+                'desktop': False
+            },
             delay=10
         )
         self._init_args()
+        self.is_mul_proc = is_mul_proc
     
     def run(self, url: str):
-        self.tag = url.split('/')[-1]
+        #####   ========================== 동영상 다운로드 주소 초기화 =============================
+        self._init_args()
+
+        self.tag = url.split('/')[-1].replace('-uncensored-leak', '')
+        if not self.is_mul_proc:
+            print(f'Downloading {self.tag} start!')
         #####   ========================== 동영상 정보 획득 =============================
         soup = self._get_html(url)
         self.title = soup.find('title').text
@@ -48,9 +60,6 @@ class MissavDownloader():
         self._save_middle_video(byte_datas, last_idx, step)
         self._save_concated_video()
 
-        #####   ========================== 동영상 다운로드 주소 초기화 =============================
-        self._init_args()
-
     def _init_args(self):
         self.title = ''
         self.last_idx = -1
@@ -58,21 +67,27 @@ class MissavDownloader():
         self.download_uri = None
     
     def _get_html(self, url: str) -> BeautifulSoup:
-        sku_url = url.split('/')[-1]
-        atmp = 0
+        sku_url = url.split('/')[-1].replace(UNCENSORED_TAG, '')
+        url = url if url.endswith(UNCENSORED_TAG) else url + UNCENSORED_TAG
+
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0',
+            'Connection': 'keep-alive',
+            'Accept': '*/*',
         }
         while True:
             for attemp in range(3):
                 response = self.scraper.get(url, headers=headers)
+                print(response, url)
+                if response.status_code == 404:
+                    url = url[:-len(UNCENSORED_TAG)]
+                    continue
                 if response.status_code != 200:
                     continue
                 soup = BeautifulSoup(response.text, 'html.parser')
                 sku_soup = soup.find('title').text.split()[0].lower()
                 if sku_soup == sku_url:
                     return soup
-            print('sleep...', atmp := atmp + 1)
             time.sleep(1)
     
     def _get_url_eval_func(self, soup: BeautifulSoup) -> str | None:
@@ -81,7 +96,7 @@ class MissavDownloader():
         target_script = next((s for s in scripts if 'source' in s.text), None)
         if not target_script:
             return None
-        
+
         match = re.search(r'eval.+', target_script.text)
         if not match:
             return None
@@ -109,9 +124,10 @@ class MissavDownloader():
     
     def _download_video_raw(self, last_idx: int) -> list[bytes]:
         byte_datas = [b''] * (last_idx+1)
-        with ThreadPoolExecutor(16) as executor:
+        with ThreadPoolExecutor(32 if self.is_mul_proc else 128) as executor:
             futures = [ executor.submit(self._fetch_video, byte_datas, idx) for idx in range(last_idx + 1) ]
-            for _ in tqdm(as_completed(futures), total=len(futures), desc=f"Downloading Raw    - {self.tag}"):
+            tqdm_loop = as_completed(futures) if self.is_mul_proc else tqdm(as_completed(futures), total=len(futures), desc=f"Downloading Raw    - {self.tag}")
+            for _ in tqdm_loop:
                 pass
         return byte_datas
     
@@ -126,46 +142,63 @@ class MissavDownloader():
             os.makedirs('./output', exist_ok=True)
 
     def _save_middle_video(self, byte_datas: list, last_idx: int, step: int):
-        #####   20분씩 자른 동영상 저장
-        temp_file_paths = []
-        max_step = math.ceil((last_idx+1) / step)
-        for i in tqdm(range(max_step), desc=f"Processing Sub mp4 - {self.tag}"):
-            temp_file_name = f'{self.tag}_{i}.mp4'
+        def save(byte_datas: list, tag: str, idx: int):
+            def read_stderr(stream):
+                try:
+                    if stream is None:
+                        return
+                    for line in io.TextIOWrapper(stream, encoding='utf-8'):
+                        pass
+                except Exception as e:
+                    pass
+
+            nonlocal temp_file_paths
+            temp_file_name = f'{tag}_{idx}.mp4'
             temp_file_path = f'./temp/{temp_file_name}'
+            # 오류나면 -c:v libx264 -c:a aac 로 변경
             ffmpeg_command = [
                 'ffmpeg',
                 '-y',
-                '-hwaccel', HWA_ACCEL,
                 '-i', '-',
-                '-c:v', 'h264_nvenc',
-                '-c:a', 'copy',
+                '-c', 'copy',
                 '-f', 'mp4',
                 temp_file_path
             ]
             process = subprocess.Popen(
                 ffmpeg_command,
                 stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
+                stdout=subprocess.DEVNULL,
                 stderr=subprocess.PIPE
             )
 
-            for chunk in byte_datas[step*i:step*(i+1)]:
+            if process.stderr:
+                threading.Thread(target=read_stderr, args=(process.stderr,), daemon=True).start()
+
+            for chunk in byte_datas:
                 process.stdin.write(chunk)
                 process.stdin.flush()
             process.stdin.close()
-
-            stdout, stderr = process.communicate()
+            
+            _, stderr = process.communicate()
 
             try:
                 if process.returncode != 0:
-                    print(f"Error occurred: {stderr.decode()}")
-                    break
+                    if not self.is_mul_proc:
+                        print(f"Error occurred: {stderr.decode()}", flush=True, end='\n\n')
+                    return
             finally:
-                del process, stdout, stderr
-                gc.collect()
-
-            temp_file_paths.append(f"file '{temp_file_name}'")
-
+                del stderr, process
+            
+            temp_file_paths[idx] = f"file '{temp_file_name}'"
+        #####   20분씩 자른 동영상 저장
+        max_step = math.ceil((last_idx+1) / step)
+        temp_file_paths = [None] * max_step
+        with ThreadPoolExecutor(8) as executor:
+            futures = [ executor.submit(save, byte_datas[step*idx:step*(idx+1)], self.tag, idx) for idx in range(max_step) ]
+            tqdm_loop = as_completed(futures) if self.is_mul_proc else tqdm(as_completed(futures), total=len(futures), desc=f"Processing  Raw    - {self.tag}")
+            for _ in tqdm_loop:
+                pass
+        
         #####   합치기 위한 동영상 목록
         with open(f'./temp/{self.tag}.txt', 'w') as f:
             f.write("\n".join(temp_file_paths))
@@ -179,10 +212,11 @@ class MissavDownloader():
             '-i', f'./temp/{self.tag}.txt',
             '-c', 'copy',
             f'./output/{re.sub(r'[\\/:"*?<>|]+', '', self.title)}.mp4'
-        ], stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+        ], stderr=subprocess.PIPE)
 
         if result.returncode != 0:
-            print(f"Error occurred: {result.stderr.decode()}")
+            if not self.is_mul_proc:
+                print(f"Error occurred: {result.stderr.decode()}")
         else:
             with open(f'./temp/{self.tag}.txt', 'r') as f:
                 files = [ filename.strip().replace("'", '').split()[1] for filename in f.readlines() if filename.strip() ]
@@ -192,12 +226,58 @@ class MissavDownloader():
                 else:
                     os.remove(file)
             os.remove(f'./temp/{self.tag}.txt')
+            if not self.is_mul_proc:
+                print(f'Save {self.tag} finished!')
+
+def mulProcDownload(urls: list[str]):
+    def _proc(url: str):
+        MissavDownloader(is_mul_proc=True).run(url)
+
+    with ProcessPoolExecutor(4) as executor:
+        future_to_tag = {
+            executor.submit(_proc, url): url.split('/')[-1].replace(UNCENSORED_TAG, '')
+            for url in urls
+        }
+        for future in tqdm(as_completed(future_to_tag), total=len(future_to_tag)):
+            try:
+                future.result()
+            except Exception as e:
+                print(f'Error occured: {e}')
+
+def singProcDownload(urls: list[str]):
+    for url in urls:
+        MissavDownloader().run(url)
+    
+def normalize_url_get_host(url):
+    if not re.match(r'^https?://', url):
+        url = 'https://' + url
+    parsed = urlparse(url)
+    return parsed.scheme + '://' + parsed.netloc
 
 if __name__ == "__main__":
-    with open('./download-list.txt', 'r') as f:
-        urls = [ line.strip() for line in f.readlines() ]
-    downloader = MissavDownloader()
-    for url in urls:
-        downloader.run(url)
+    download_file_path = './download-list.txt'
+    with open(download_file_path, 'r') as f:
+        urls = list(dict.fromkeys( line.strip() for line in f.readlines() if line.strip() ))
 
-    # subprocess.Popen(['ffmpeg', '-hwaccels'])
+    if len(urls) == 0:
+        exit(-1)
+    
+    hosts = list(set(normalize_url_get_host(url) for url in urls))
+
+    error_hosts = []
+    for host in hosts:
+        try:
+            requests.get(host)
+        except requests.exceptions.ConnectionError as ce:
+            error_hosts.append(host)
+    if len(error_hosts) > 0:
+        print('접근 불가능한 링크')
+        error_hosts = map(lambda x: f'- {x}', error_hosts)
+        print('\n'.join(error_hosts))
+
+    # mulProcDownload(urls)
+
+    singProcDownload(urls)
+
+    with open(download_file_path, 'w') as f:
+        f.write('')
