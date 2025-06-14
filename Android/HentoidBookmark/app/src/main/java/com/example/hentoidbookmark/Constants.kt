c6647220a70f8eedd882c27f8b4311a01fd49e69
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-14 14:08:22 +0900

    구글 좆병신새끼

--- Android/HentoidBookmark/app/src/main/java/com/example/hentoidbookmark/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hentoidbookmark
-
-object Constants {
-    const val SITE_HITOMI = "HITOMI"
-    const val DOMAIN2 = "gold-usergeneratedcontent.net"
-    const val DOMAIN = "ltn.$DOMAIN2"
-    const val NOZOMI_EXTENSION = ".nozomi"
-    const val COMPRESSED_NOZOMI_PREFIX = "n"
-}
